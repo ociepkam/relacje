d19356d21003a85dee404f54db88acb60bbaf12d
Jan <jastrz.jan@gmail.com>
2018-10-10 16:38:06 +0200

    klawisze instrukcja kolorowe kwadraty

--- sources/parameters.py
+++ sources/parameters.py
@@ -1,3 +1,3 @@
 # change second number to change color
-FIGURES = ["{}_{}.png".format(i+1, 11) for i in range(25)]
-KEYS = ['z', 'x', 'c']
+FIGURES = ["{}_{}.png".format(3, i) for i in [1,7,8,9]]
+KEYS = ['left', 'down', 'right']
